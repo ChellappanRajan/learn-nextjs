8bddba147b6ebc438bf2b7a1fc887a998fa93bc6
ChellappanRajan <vrchellappan@gmail.com>
2025-04-01 20:37:00 +0530

    Add QueryProvider and integrate React Query for data fetching

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeContext, ThemeProvider } from "@/providers/ThemeProvider";
+import { QueryProvider } from "@/providers/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider value="light">{children}</ThemeProvider>
+        <QueryProvider>
+          <ThemeProvider value="light">{children}</ThemeProvider>
+        </QueryProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,40 @@
 import { useContext } from "react";
 import { send } from "./actions";
 import { ThemeContext } from "@/providers/ThemeProvider";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
   const theme = useContext(ThemeContext);
-  // let data = await fetch("http://localhost:3000/api");
-  // let json = await data.json();
-  console.log(theme);
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://api.github.com/repos/TanStack/query",
+      );
+      return await response.json();
+    },
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
-    <form action={send}>
-      <input type="text" />
-      <button type="submit">Send</button>
-    </form>
+    <main>
+      <form action={send}>
+        <input type="text" />
+        <button type="submit">Send</button>
+      </form>
+      <section>
+        <div>
+          <h1>{data.full_name}</h1>
+          <p>{data.description}</p>
+          <strong>👀 {data.subscribers_count}</strong>{" "}
+          <strong>✨ {data.stargazers_count}</strong>{" "}
+          <strong>🍴 {data.forks_count}</strong>
+          <div>{isFetching ? "Updating..." : ""}</div>
+        </div>
+      </section>
+    </main>
   );
 }
